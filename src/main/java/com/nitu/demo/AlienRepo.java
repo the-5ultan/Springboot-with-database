2e5e3f214bee96d30e1d70db89d046bba41e25a7
the-5ultan <rfgamers2005@gmail.com>
2026-07-09 15:51:26 +0500

    added a direct self writen querry in the interface AlienRepo to get all the names and id's for a specific name searched

--- src/main/java/com/nitu/demo/AlienRepo.java
+++ src/main/java/com/nitu/demo/AlienRepo.java
@@ -2,6 +2,9 @@ package com.nitu.demo;
 
 //import org.jspecify.annotations.Nullable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+//import org.springframework.web.bind.annotation.RequestParam;
 
 import com.nitu.demo.models.Alien;
 import java.util.List;
@@ -10,6 +13,9 @@ import java.util.List;
 public interface AlienRepo extends JpaRepository<Alien, Integer>{
 
 
-	List<Alien> findByNameOrderById(String name); // Querry DSL
+	List<Alien> findByNameOrderByIdDesc(String name); // Querry DSL
+	
+	@Query("SELECT a FROM Alien a WHERE a.name = :name")
+	List<Alien> find(@Param("name") String name);
 	
 }
