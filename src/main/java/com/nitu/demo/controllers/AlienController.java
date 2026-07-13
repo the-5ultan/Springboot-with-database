04ea34dcaa8165416ed1760c0460c10b0b147cb6
Fasihuddin <sultanffyt09@gmail.com>
2026-07-13 16:48:06 +0500

    ALien Controller setted up so now the data at get (aliens) request is in JSON format.

--- src/main/java/com/nitu/demo/controllers/AlienController.java
+++ src/main/java/com/nitu/demo/controllers/AlienController.java
@@ -2,19 +2,25 @@ package com.nitu.demo.controllers;
 
 
 import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 //import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 import com.nitu.demo.AlienRepo;
 import com.nitu.demo.models.Alien;
 
 
 @Controller
 public class AlienController {
+	@Autowired
 	AlienRepo repo;
 	@GetMapping("/aliens")
-    public String getAliens() {
+	@ResponseBody
+    public List<Alien> getAliens() {
     	List<Alien> aliens = repo.findAll();
-    	return aliens.toString();
+    	return aliens;
     }
 }
