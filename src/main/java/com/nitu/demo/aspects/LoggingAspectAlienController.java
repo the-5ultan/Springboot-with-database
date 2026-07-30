1108c6e9bfb9510bce271df22a10c56b01268468
the-5ultan <rfgamers2005@gmail.com>
2026-07-30 14:18:49 +0500

    Logger introduced instead of the normal System.out.println statements

--- src/main/java/com/nitu/demo/aspects/LoggingAspectAlienController.java
+++ src/main/java/com/nitu/demo/aspects/LoggingAspectAlienController.java
@@ -3,20 +3,24 @@ package com.nitu.demo.aspects;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class LoggingAspectAlienController {
 
+    private static final Logger logger = LoggerFactory.getLogger(LoggingAspectAlienController.class);
+
     @Before("execution(public * com.nitu.demo.controllers.AlienController.getAliens())")
     public void getAliensBeforeLog(){
-        System.out.println("GetAliens() method called !");
+        logger.info("GetAliens() method called!");
     }
 
     @After("execution(public * com.nitu.demo.controllers.AlienController.getAliens())")
     public void getAliensAfterLog(){
-        System.out.println("GetAliens() method Executed !");
+        logger.info("GetAliens() method Executed!");
     }
 
 
